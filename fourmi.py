59eb16c97f2c9a8776dd0ad240f10cf5f80c8503
Eleonore Douet <eleonore.douet@icloud.com>
2026-01-30 11:33:05 +0100

    'ajout calcul distance'

--- fourmi.py
+++ fourmi.py
@@ -2,8 +2,7 @@
 
 class AntColony:
 
-    def __init__(self, distances, n_fourmis, n_meilleurs, n_iterations, decroissance, alpha : float=1, beta : float =2):
-        
+    def __init__(self, distances, n_fourmis, n_meilleurs, n_iterations, decroissance, alpha : float=1, beta : float =2): 
         self.distances = distances
         self.pheromones = [[1.0 for _ in range(len(distances))] for _ in range(len(distances))]
         self.n_fourmis = n_fourmis
@@ -15,3 +14,11 @@ class AntColony:
         self.tous_indices = range(len(distances))
         self.meilleur_chemin = None
         self.meilleure_distance = float('inf')
+    
+    def calcul_distance_chemin(self, chemin) : 
+        total = 0
+        for i in range(len(chemin) - 1):
+            total += self.distances[chemin[i]][chemin[i+1]]
+        return total
+    
+    
