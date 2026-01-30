2096bb7d2a5fa01a7843ced600a2896441533178
Mona Sidhoum <mona.sidhoum@etu.minesparis.psl.eu>
2026-01-30 11:54:34 +0100

    suite de la classe fourmi

--- fourmi.py
+++ fourmi.py
@@ -1,4 +1,4 @@
-
+import random
 
 class AntColony:
 
@@ -24,20 +24,39 @@ class AntColony:
     def calculer_probabilites_mouvement(self, chemin):
         actuelle = chemin[-1]
         probabilites = []
-        # Pour chaque ville, calculer la probabilité de mouvement
         for ville in self.tous_indices:
-            # Si la ville est dans le chemin, la probabilité est nulle
             if ville in chemin:
                 probabilites.append(0)
             else:
-                # Phéromone^alpha * (1/distance)^beta
                 pheromone = self.pheromones[actuelle][ville] ** self.alpha
-                heuristique = (1.0 / self.distances[actuelle][ville]) ** self.beta
-                # Ajouter la probabilité au tableau
-                probabilites.append(pheromone * heuristique)
-        # Sommer les probabilités pour la normalisation
-        total = sum(probabilites)
-        # Retourner les probabilités normalisées
+                heuristique = (1.0 / self.distances[actuelle][ville]) ** self.beta 
+                probabilites.append(pheromone * heuristique) # Phéromone^alpha * (1/distance)^beta
+        total = sum(probabilites) # pour la normalisation
         return [p / total for p in probabilites] if total > 0 else [0] * len(probabilites)
+    
+    def choisir_ville_suivante(self, probabilites):
+        r = random.random()
+        total = 0
+        for i, p in enumerate(probabilites):
+            total += p
+            if total >= r:
+                return i
+        return len(probabilites) - 1
+    
+    def deposer_pheromones(self, tous_chemins):
+        chemins_tries = sorted(tous_chemins, key=lambda x: x[1]) #car tous_chemins est une liste de tuple qui contient un chemin et sa distance totale et on trie en fonction de la distance
+        for chemin, distance in chemins_tries[:self.n_meilleurs]:
+            for i in range(len(chemin) - 1):
+                self.pheromones[chemin[i]][chemin[i+1]] += 1.0 
+    
+    def generer_tous_chemins(self):
+        tous_chemins = []
+        for _ in range(self.n_fourmis):
+            chemin = [random.randint(0, len(self.distances) - 1)] # on commence par une ville aléatoire
+            while len(chemin) < len(self.distances):
+                probabilites_mouvement = self.calculer_probabilites_mouvement(chemin)
+                ville_suivante = self.choisir_ville_suivante(probabilites_mouvement)
+                chemin.append(ville_suivante)
+            tous_chemins.append((chemin, self.calcul_distance_chemin(chemin)))
     
     
