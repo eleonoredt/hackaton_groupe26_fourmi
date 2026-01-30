333f550251930578f4c1a7c89c83b50a34bc92f4
Leah Toledano <leah.toledano@etu.minesparis.psl.eu>
2026-01-30 10:58:21 +0100

    creation problem

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ from dataclasses import dataclass
 
 import numpy as np
 import pandas as pd
+from problem import Problem2D
+
+problem = Problem2D("data/video-06.csv")
 
 class Fourmi:
+
     def __init__(self):
+        
         
         
---
+++ problem.py
@@ -0,0 +1,7 @@
+
+
+class Problem2D:
+
+    def __init__(self, nodes: list, distances: np.ndarray):
+        self.nodes = nodes
+        self.distances = distances
